3bddd1d9c7519d2a5f18d33789ec739f3e21297a
Денис <mden1996.07@gmail.com>
2024-03-13 01:26:59 +0300

    Добавьте файлы проекта.

---
+++ Task/Equation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    struct Equation
+    {
+        double k;
+        double b;
+
+        public Equation(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+        public double Root()
+        {
+            double res = -b / k;
+            return res;
+        }
+    }
+}
---
+++ Task/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите значение для b и для k для уровнения 0=kx+b");
+            double k = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            Equation equation = new Equation(k, b);
+            double root = equation.Root();
+            Console.WriteLine("Результат вычисления корная уравнения x = {0}", root);
+            Console.ReadKey();
+
+        }
+    }
+}
